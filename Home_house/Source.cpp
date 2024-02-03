de6bf3f052fe9f956be6cbef041d38142ef0eeab
Слава <@KOMPUTER>
2024-02-03 23:47:19 +0500

    Что-то не то

--- Home_house/Source.cpp
+++ Home_house/Source.cpp
@@ -49,12 +49,12 @@ class Flat : public Human {
 protected:
     int flat_number;
     int size_; // кол-во человек в квартире
-  
+    Human* obj;
 public:
     Flat() : Human() {
         flat_number = 0;
         int size_ = 0;
-        Human *obj = new Human [size_] {};
+        
     } 
     
     
@@ -62,23 +62,18 @@ public:
     Flat(int flat_number, int size_, Human* obj) : Human(name, age) {
         this->flat_number = flat_number;
         this->size_ = size_;
-       
+        this->obj = new Human [size_] {};
+      
     }
-
-   
-   
 
    void printFlat() {
          std::cout << "Квартира " << flat_number << '\n';
          std::cout << "Проживает " << size_ <<  "человек" << '\n';
          std::cout << std::endl;
-    }
-   
-  
-   ~Flat() {  delete[] obj; 
-        
-    }
-       
+         }
+         
+    
+    ~Flat() {delete[] obj;}
 };
  
    
@@ -96,5 +91,6 @@ int main()
     flat1.printFlat();
     obj1.printhuman();
     obj2.printhuman();
+   
     return 0;
 }
