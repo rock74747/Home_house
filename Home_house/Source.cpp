fb564ecf77a8daa2e350d28b971a24f519ba7f52
Слава <@KOMPUTER>
2024-02-06 19:00:28 +0500

    Переделал

--- Home_house/Source.cpp
+++ Home_house/Source.cpp
@@ -1,96 +1,132 @@
 ﻿#include <iostream>
 #include <string>
 
+
 class Human {
-protected:
+private:
     std::string name;
     int age;
+    
 public:
-    Human() {
-        name = "noname";
-        age = 0;
-    }
-
-    Human(std::string name, int age) {
+    
+    Human(std::string name = "noname", int age = 0) {
         this->name = name;
         this->age = age;
-    }
-    Human(const Human& other) {
-        this->name = other.name;
-        this->age = other.age;
-    }
-
-
-    void setName(std::string name) {
-        this->name = name;
-    }
-    std::string getName() {
-        return this->name;
-    }  
-    void setAge(int age) {
-        this->age = age;
-    }
-    int  getAge() {
-        return this->age;
     }
     
-
     void printhuman() {
-        std::cout << name << age << "лет" << '\n';
-        std::cout << std::endl;
+        std::cout << this-> name << std::endl; 
+        std::cout << this->age << " лет" << std::endl;
     }
-
-
-     ~Human() {}
+    Human (const Human& ob) {
+    this->name = ob.name;
+    this->age = ob.age;
+    }
+   ~ Human () {}
 };
-   
 
-class Flat : public Human {
-protected:
+
+
+class Flat {
+    friend class Human;
+private:
     int flat_number;
-    int size_; // кол-во человек в квартире
-    Human* obj;
+    int size_; // кол-во человек в квартире 
+    Human* ob;
 public:
-    Flat() : Human() {
-        flat_number = 0;
-        int size_ = 0;
-        
-    } 
-    
-    
-     
-    Flat(int flat_number, int size_, Human* obj) : Human(name, age) {
-        this->flat_number = flat_number;
-        this->size_ = size_;
-        this->obj = new Human [size_] {};
-      
-    }
 
-   void printFlat() {
-         std::cout << "Квартира " << flat_number << '\n';
-         std::cout << "Проживает " << size_ <<  "человек" << '\n';
-         std::cout << std::endl;
-         }
-         
+    Flat() : flat_number  (0), size_(0), ob (nullptr) {}
+
+    Flat( int flat_number, int size_,  Human& obj) {
+    ob = new Human;
+    this->ob = &obj;
+    this->size_ = size_;
+    this->flat_number = flat_number;
+    }
+        
+   
     
-    ~Flat() {delete[] obj;}
+   void printFlat() {
+         std::cout << "Квартира " << this->flat_number << '\n';
+         std::cout << "Проживает " << this->size_ <<  " человек(а)" << '\n';
+         for (int i = 0; i < size_; i++) {
+             this->ob[i].printhuman(); 
+             std::cout << '\n';
+         }
+   }
+   
+   Flat (const Flat& obj) {
+       this->ob = obj.ob;
+       this->ob = new Human;
+       this->size_ = obj.size_;
+   }
+    ~Flat() {
+       std:: cout << this << std::endl; delete[] ob;
+   }
+   
 };
  
-   
+class House
+{
+  private:
+    Flat* ob;
+    int sized_; // кол-во  квартир 
+    int number_hous; // номер дома
+public:
+    House() : number_hous (0), sized_(0), ob(nullptr) {};
+    
+    House(int number_hous, int sized_, Flat& o) {
+    this->number_hous= number_hous;
+    this->sized_ = sized_;
+    this->ob = &o;
+    }
+    House(const House& obj) {
+        this->ob = obj.ob;
+    }
+    
+    void printhouse() {
+        std::cout << "Номер дома: " << this->number_hous << '\n';
+        std::cout << "Количество квартир: " << this->sized_  << '\n';
+        for (int i = 0; i < sized_; i++) {
+            this->ob[i].printFlat();
+        }
+    }
+    ~House() {
+        std::cout << this << std::endl; 
+        delete[] ob;
+    }
+};
+
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    Human obj1 ("Петров В.В.", 25);
-    Human obj2("Петрова С.В.", 22);
+     
+  
+    Human* arr1 = new Human [2];
+    Flat flat1 (1, 2, *arr1);
+    arr1[0] = { "Петров В.В.", 25 }; 
+    arr1[1] = { "Петрова С.В.", 22 };
    
- 
-    Human* obj[] = {&obj1, &obj2};
-
-    Flat flat1 (1, 2, obj[2]);
-    flat1.printFlat();
-    obj1.printhuman();
-    obj2.printhuman();
+    
+     Human* arr2 = new Human[3];
+     Flat flat2(2, 3, *arr2); 
+     arr2[0] = { "Семенов В.В.", 15 };
+     arr2[1] = { "Семенов С.В.", 29 };
+     arr2[2] = { "Иванов Л.В.", 74 };
+     
+     Flat* arr = new Flat[2];
+     House house (55,2, *arr);
+     arr[0] = { flat1 };
+     arr[1] = { flat2 };
+     
+     house.printhouse();
+     
+     delete[] arr1;
+     delete[] arr2;
+     delete[] arr;
+     arr = nullptr;
    
+  
     return 0;
 }
